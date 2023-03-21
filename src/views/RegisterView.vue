c701d31633216b84c06e9464d645132def9f1776
lauchach <tarnapot.b38@gmail.com>
2023-03-22 00:15:48 +0700

    [edit] css app page and css register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,20 +1,64 @@
 <template>
+  REGISTER
+      <div class="dev1">
+        <div class="row">
+          <div class="col">
+            <label for="username">Username:</label>
+          </div>
+          <div class="col">
+          <input type="text" id="username" v-model="username" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <label for="username">Username:</label>
+          </div>
+          <div class="col">
+          <input type="text" id="username" v-model="username" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            1 of 3
+          </div>
+          <div class="col">
+            2 of 3
+          </div>
+          <div class="col">
+            3 of 3
+          </div>
+        </div>
+      </div>
   <div class="about">
-    <h1>REGISTER</h1>
     <div>
-    <h1>Login Page</h1>
-    <form>
-      <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" v-model="username" />
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" />
-      </div>
-      <button type="submit" @click.prevent="login">Login</button>
-    </form>
-  </div>
+      <form>
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" id="username" v-model="username" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <button type="submit" @click.prevent="login">Login</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -30,18 +74,27 @@ export default {
     login() {
       // perform login logic here
       console.log(`Username: ${this.username} Password: ${this.password}`);
+      if (this.username === '1') this.$router.push("/");
       // ไป หา ยูสว่าตรงมั๊ยในถังถ้าไม่ตรงให้ save
     },
   },
 };
 </script>
-
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+  @media (max-width: 720px) {
+  .div1 {
+    width: 100%;
+    height: 900px;
+    padding: 0px;
+    margin: auto;
+    border: 1px solid yellow;
+  }
+  .div1 .row {
+    border: 1px solid yellow;
+  }
+
+  .div1 .row .col {
+    border: 1px solid orange;
   }
 }
 </style>
